6a0e00b935e55aef2c4da8285291cf6b126ce7e2
FrancisTan <499166435@qq.com>
2016-09-09 10:57:07 +0800

    增加nav-status.js，修复左侧菜单在页面中转后不自动展开的问题

---
+++ Guoli.Admin/Scripts/page-scripts/nav-status.js
@@ -0,0 +1,25 @@
+﻿/// <reference path="../plugins/jquery.cookie/jquery.cookie.js" />
+(function() {
+    $(function () {
+        var url = location.href;
+        var indexOfParamsStart = url.indexOf('?');
+        if (indexOfParamsStart === -1) {
+            indexOfParamsStart = url.length - 1;
+        }
+        
+        var $currentMenu = null;
+        $('#navigation a').each(function() {
+            var href = $(this).attr('href');
+            var index = url.indexOf(href);
+            if (index > 0 && index < indexOfParamsStart) {
+                $currentMenu = $(this);
+            } 
+        });
+
+        if ($currentMenu) {
+            $currentMenu.parent('li').addClass('active')
+                .parent('ul').css('display', 'block')
+                .parent('li').addClass('active open');
+        }
+    });
+})();
