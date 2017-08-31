deb84b9aac607fe3e8b01355c788f037989ed451
FrancisTan <499166435@qq.com>
2017-08-31 23:00:27 +0800

    增加移动端操作日志上传功能
    增加表 AppOperateLog
    增加文件 AppOperateLog.cs
    增加文件 AppOperateLogDal.cs
    增加文件 AppOperateLogBll.cs

---
+++ Guoli.Model/Partital/AppOperateLog.cs
@@ -0,0 +1,71 @@
+﻿using System;
+namespace Guoli.Model
+{
+    /// <summary>
+    /// App操作日志
+    /// </summary>
+    [Serializable]
+    public partial class AppOperateLog
+    {
+        public AppOperateLog()
+        { }
+        #region Model
+        private int _id;
+        private int _logtype;
+        private string _logcontent;
+        private int _operatorid;
+        private int _deviceid;
+        private DateTime _addtime = DateTime.Now;
+        /// <summary>
+        /// 主键
+        /// </summary>
+        public int Id
+        {
+            set { _id = value; }
+            get { return _id; }
+        }
+        /// <summary>
+        /// 操作类型（1.登录系统; 2.退出系统;）
+        /// </summary>
+        public int LogType
+        {
+            set { _logtype = value; }
+            get { return _logtype; }
+        }
+        /// <summary>
+        /// 日志内容
+        /// </summary>
+        public string LogContent
+        {
+            set { _logcontent = value; }
+            get { return _logcontent; }
+        }
+        /// <summary>
+        /// 操作者Id
+        /// </summary>
+        public int OperatorId
+        {
+            set { _operatorid = value; }
+            get { return _operatorid; }
+        }
+        /// <summary>
+        /// 设备Id，关联设备表的主键
+        /// </summary>
+        public int DeviceId
+        {
+            set { _deviceid = value; }
+            get { return _deviceid; }
+        }
+        /// <summary>
+        /// 添加时间
+        /// </summary>
+        public DateTime AddTime
+        {
+            set { _addtime = value; }
+            get { return _addtime; }
+        }
+        #endregion Model
+
+    }
+}
+
